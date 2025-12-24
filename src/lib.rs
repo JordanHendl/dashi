7982c3df39fcc16d64f18c47ee88d71e21bb8622
Jordan Hendl <jordiehendl@gmail.com>
2025-12-23 21:25:09 -0600

    add fill macro

--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,23 @@ compile_error!(
     "window backends are mutually exclusive; enable only one of `dashi-sdl2`, `dashi-minifb`, or `dashi-winit`"
 );
 
+#[macro_export]
+macro_rules! fill {
+    // Prefix values, then ".. fill", then "; N"
+    ($($val:expr),+ , .. $fill:expr ; $len:expr $(,)?) => {{
+        let mut arr = [$fill; $len]; // requires T: Clone if not Copy
+        let mut i = 0usize;
+        $(
+            arr[i] = $val;
+            i += 1;
+        )+
+        arr
+    }};
+
+    // Just fill: arrfill![..fill; N]
+    (.. $fill:expr ; $len:expr $(,)?) => {{
+        [$fill; $len]
+    }};
+}
 
 pub use gpu::*;
