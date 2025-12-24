50daef8606453853c132b7bcfa0cbfab6136f154
Jordan Hendl <jordiehendl@gmail.com>
2025-12-23 21:34:25 -0600

    update fill macro

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,25 @@ compile_error!(
 
 #[macro_export]
 macro_rules! fill {
-    // Prefix values, then ".. fill", then "; N"
-    ($($val:expr),+ , .. $fill:expr ; $len:expr $(,)?) => {{
-        let mut arr = [$fill; $len]; // requires T: Clone if not Copy
-        let mut i = 0usize;
-        $(
-            arr[i] = $val;
-            i += 1;
-        )+
-        arr
-    }};
-
-    // Just fill: arrfill![..fill; N]
+    // Only fill: fill![..x; N]
     (.. $fill:expr ; $len:expr $(,)?) => {{
         [$fill; $len]
+    }};
+
+    // Prefix tokens up to `..`, then fill and length.
+    ($($prefix:tt)+ .. $fill:expr ; $len:expr $(,)?) => {{
+        let mut __arr = [$fill; $len];
+        $crate::fill!(@set __arr, 0usize, $($prefix)+);
+        __arr
+    }};
+
+    // Accept an empty prefix (optional convenience): fill![..x; N]
+    (@set $arr:ident, $i:expr $(,)?) => {};
+
+    // Munch `expr, expr, expr, ...` and assign into the array.
+    (@set $arr:ident, $i:expr, $head:expr $(, $tail:expr)* $(,)?) => {{
+        $arr[$i] = $head;
+        $crate::fill!(@set $arr, $i + 1usize, $($tail),*);
     }};
 }
 
